16a87d50b98062936b228a87e3ca60b0fe7d7ea3
Aman Sharma <98725523+amansharma049@users.noreply.github.com>
2023-10-26 21:24:04 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// function img(){
+    
+// }
+function closeImg(){
+    document.getElementById('popup-reg').classList.remove('active');
+}
+
+var MainImg = document.getElementById("MainImg");
+    var smallimg = document.getElementsByClassName("small-img");
+
+    smallimg[0].onclick = function() {
+        MainImg.src = smallimg[0].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[1].onclick = function() {
+        MainImg.src = smallimg[1].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[2].onclick = function() {
+        MainImg.src = smallimg[2].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[3].onclick = function() {
+        MainImg.src = smallimg[3].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[4].onclick = function() {
+        MainImg.src = smallimg[4].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[5].onclick = function() {
+        MainImg.src = smallimg[5].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[6].onclick = function() {
+        MainImg.src = smallimg[6].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[7].onclick = function() {
+        MainImg.src = smallimg[7].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
+    smallimg[8].onclick = function() {
+        MainImg.src = smallimg[8].src;
+        document.getElementById('popup-reg').classList.add('active');
+    }
